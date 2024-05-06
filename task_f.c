6b5d805a8088ccf3e09308e00c2f6de801a2807f
Yufu-W <160229997+Yufu-W@users.noreply.github.com>
2024-05-06 15:38:03 +0100

    Add files via upload

---
+++ task_f.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include "domains.h"
+
+// Main function
+int main(void) {
+
+	// Read user input
+#define LEN 4
+char user_input[LEN];
+printf("Enter a domain including the . (e.g. .uk): ");
+fgets(user_input, LEN, stdin);
+
+if (user_input[0] != '.') {
+	printf("Error! The first character entered should be a .");
+	return 1;
+}
+// Enter your code under here
+//  Use strcmp(user_input, country_codes[0],domain) to compare the text in user input to the text in country_codes[0].domain
+user_input[strcspn(user_input, "\n")] = 0;
+
+int found = 0; 
+for (int i = 0; i < sizeof(country_codes) / sizeof(country_codes[0]); i++) {
+if (strcmp(user_input, country_codes[i].domain) == 0) {
+ printf("This is the domain for: %s", country_codes[i].country);
+found = 1;
+break;
+	}
+ }
+
+// If no match is found, print a prompt message
+ if (!found) {
+	printf("Domain not found.");
+ }
+
+	 //Do not edit below here
+	return 0;
+ }
